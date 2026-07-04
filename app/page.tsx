29c8d18ad2e1a983c80897a52e32fd22e5b5f484
mdfarhan1506 <md_ua2503aih128@iitp.ac.in>
2026-07-04 19:38:11 +0530

    Fix routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
-import Welcome from "../components/welcome/Welcome";
-import Envelope from "../components/envelope/Envelope";
-import Cake from "../components/cake/Cake";
-import Wish from "../components/wish/Wish";
-import Music from "../components/music/Music";
-import Cards from "../components/cards/Cards";
-import FinalLetter from "../components/final-letter/FinalLetter";
+import Welcome from "@/components/welcome/Welcome";
 
 export default function Home() {
-  const [page, setPage] = useState(0);
-
-  switch (page) {
-    case 0:
-      return <Welcome onNext={() => setPage(1)} />;
-
-    case 1:
-      return <Envelope onNext={() => setPage(2)} />;
-
-    case 2:
-      return <Cake onNext={() => setPage(3)} />;
-
-    case 3:
-      return <Wish onNext={() => setPage(4)} />;
-
-    case 4:
-      return <Music onNext={() => setPage(5)} />;
-
-    case 5:
-      return <Cards onNext={() => setPage(6)} />;
-
-    default:
-      return <FinalLetter />;
-  }
+  return <Welcome />;
 }
